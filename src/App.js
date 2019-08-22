fe4cae97926752bcd0744fae0c40137d7e16c5f2
Alex <alexwilsonaqnu@gmail.com>
2019-08-22 10:42:25 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,49 +1,18 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
-import FacebookLoginButton from './FacebookLoginButton';
+import FacebookLogin from 'react-facebook-login';
 
-class App extends Component {
-
-  state = {
-    username: null
-  };
-
-  onFacebookLogin = (loginStatus, resultObject) => {
-    if (loginStatus === true) {
-      this.setState({
-        username: resultObject.user.name
-      });
-    } else {
-      alert('Facebook login error');
-    }
-  }
-
-  render() {
-    const { username } = this.state;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">React Social Media Login</h1>
-        </header>
-
-        <div className="App-intro">
-          { !username &&
-            <div>
-              <p>Click on one of any button below to login</p>
-              <FacebookLoginButton onLogin={this.onFacebookLogin}>
-                <button>Facebook</button>
-              </FacebookLoginButton>
-            </div>
-          }
-          {username &&
-            <p>Welcome back, {username}</p>
-          }
-        </div>
-      </div>
-    );
-  }
+const responseFacebook = (response) => {
+  console.log(response);
 }
 
-export default App
+ReactDOM.render(
+  <FacebookLogin
+    appId="2410340132393995"
+    autoLoad={true}
+    fields="name,email,picture"
+    onClick={componentClicked}
+    callback={responseFacebook} />,
+  document.getElementById('demo')
+);
