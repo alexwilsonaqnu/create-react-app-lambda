f9ecfc2a771f1d8a21f1c56397462eeaa4d8532a
Alex <alexwilsonaqnu@gmail.com>
2019-08-22 13:22:53 -0500

    Updates for Google Login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-import FacebookLogin from 'react-facebook-login';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const responseFacebook = (response) => {
-  console.log(response);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-ReactDOM.render(
-  <FacebookLogin
-    appId="2410340132393995"
-    autoLoad={true}
-    fields="name,email,picture"
-    onClick={componentClicked}
-    callback={responseFacebook} />,
-  document.getElementById('demo')
-);
+export default App;
